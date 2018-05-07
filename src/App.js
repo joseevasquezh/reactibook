f4b32fa53ec9c67c35db3b4fccb93e37a17e69a2
Jose Vasquez <josee.vasquezh@gmail.com>
2018-05-07 01:06:55 -0400

    Add post message feature
    After creating the store in the index.js I gathered
    some of my testing react code in App.js to test the
    dispatch method and add some posts.
    
    All this code in App.js needs some refactoring, also
    some testing method are required.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,60 @@
 import React, { Component } from 'react';
-import { createStore } from 'redux';
-import wallApp from './Reducers';
+
+import Post from './components/Post'
 import './App.css';
 
-
-const store = createStore(wallApp);
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <Wall posts={this.props.posts} onClickPost={(id,text,visibility) => this.props.onClickPost(id,text,visibility)}/>
+    );
+  }
+}
 
+class Wall extends Component {
+  render () {
+    return (
+      <div className="wall">
+        <PostForm onClickPost={(id,text,visibility) => this.props.onClickPost(id,text,visibility)}/>
+        {this.props.posts.map(post =>
+          <Post
+          key={post.id}
+          id={post.id}
+          text={post.text}
+          onClickSave={ ()=> console.log('save clicked') }
+          onClickDelete={ ()=> console.log('delete clicked') }
+          />
+        )}
       </div>
     );
   }
 }
+
+let nextPostId = 0;
+class PostForm extends Component {
+  render () {
+    return (
+      <div>
+        <form
+            onSubmit={e => { e.preventDefault()
+        }}>
+          <input
+            ref={node => {this.input = node}}
+          />
+          <select ref={node => {this.select = node}}>
+            <option value="public">Publico</option>
+            <option value="friends" >Amigos</option>
+          </select>
+          <button onClick={()=>this.props.onClickPost(nextPostId++, this.input.value, this.select.value)}>
+            Publicar
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,5 +4,35 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore } from 'redux';
+import wallApp from './Reducers';
+
+import {
+  addPost,
+  editPost,
+  setVisibilityFilter,
+  VisibilityFilters,
+} from './Actions';
+
+
+const store = createStore(wallApp);
+
+
+console.log(store.getState());
+
+store.subscribe(() =>
+  console.log(store.getState())
+);
+
+
+const render = () => {
+  ReactDOM.render(<App
+    posts={store.getState().posts}
+    onClickPost={(id,text,visbility)=>store.dispatch(addPost(id,text,(visbility === 'friends' ? false : true)))}
+  />, document.getElementById('root'));
+}
+store.subscribe(render);
+render();
+
+
 registerServiceWorker();
